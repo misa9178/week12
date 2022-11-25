c0ea64acd4f2a769b260542a24efb67c20e15d6c
misa9178 <113404608+misa9178@users.noreply.github.com>
2022-11-25 14:56:10 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -8,11 +8,6 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 from flask import Flask, render_template, request
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
 
 app = Flask(__name__)
 
@@ -91,6 +86,25 @@ def movie():
         doc_ref.set(doc)
     return "近期上映電影已爬蟲及存檔完畢，網站最近更新日期為：" + lastUpdate 
 
+@app.route("/search", methods=["POST","GET"])
+def search():
+    if request.method == "POST":
+        MovieTitle = request.form["MovieTitle"]
+        info = ""     
+        collection_ref = db.collection("電影")
+        #docs = collection_ref.where("title","==", "夜鷹的單戀").get()
+        docs = collection_ref.order_by("showDate").get()
+        for doc in docs:
+            if MovieTitle in doc.to_dict()["title"]: 
+                info += "片名：" + doc.to_dict()["title"] + "<br>" 
+                info += "海報：" + doc.to_dict()["picture"] + "<br>"
+                info += "影片介紹：" + doc.to_dict()["hyperlink"] + "<br>"
+                info += "片長：" + doc.to_dict()["showLength"] + " 分鐘<br>" 
+                info += "上映日期：" + doc.to_dict()["showDate"] + "<br><br>"           
+        return info
+    else:  
+        return render_template("input.html")
 
 if __name__ == "__main__":
     app.run()
+
